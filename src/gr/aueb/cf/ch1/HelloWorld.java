317f10c50e0679f1aa99864affd175be361ba9b4
Christos Tartaris <xristosbnd@gmail.com>
2024-12-10 20:01:11 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * κανει εκτυπωση το hello coding factory
+ * στην κονσολα
+ */
+
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Coding Factory");
+    }
+
+}
